dad34a10978d56ed899ce881abe2a99a9899ccf9
Anthony Langford <anthony.langford@gmail.com>
2017-01-16 23:55:57 +0000

    added console input functionality and error messages if no input

--- download_avatars.js
+++ download_avatars.js
@@ -4,9 +4,24 @@ var fs = require('fs');
 var GITHUB_USER = 'iamtonybologna';
 var GITHUB_TOKEN = 'b1d448a48b1e6f52b5303b1f899e2c7f3d179478';
 
+var owner;
+var repo;
+owner = process.argv[2];
+repo = process.argv[3];
+
 console.log('Welcome to the GitHub Avatar Downloader!');
 
+if (owner === undefined && repo === undefined) {
+  console.log('You didn\'t provide an owner and repo for me! Please run me again: node download_avatars.js <owner> <repo>');
+} else if (repo === undefined) {
+  console.log('You didn\'t provide an repo for me! Please run me again: node download_avatars.js <owner> <repo>')
+} else if (owner === undefined) {
+  console.log('You didn\'t provide an owner for me! Please run me again: node download_avatars.js <owner> <repo>')
+}
+
 function getRepoContributors(repoOwner, repoName, callback) {
+
+
   var requestURL = 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
 
   var options = {
@@ -30,7 +45,7 @@ function downloadImageByURL(url, filePath) {
     .pipe(fs.createWriteStream(filePath))
 }
 
-  getRepoContributors("jquery", "jquery", function(err, result) {
+  getRepoContributors(owner, repo, function(err, result) {
   console.log("Errors:", err);
   console.log("Result:", result);
   result.forEach(function(entry) {
