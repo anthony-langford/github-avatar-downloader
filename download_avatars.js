52ee870cc8b7b9e89611e11dc9b6acac31798f3b
Anthony Langford <anthony.langford@gmail.com>
2017-01-16 22:18:18 +0000

    fetched body and parsed it to a JSON object and logged it in console

--- download_avatars.js
+++ download_avatars.js
@@ -1,27 +1,29 @@
 var request = require('request');
-var fs = require('fs');
+
 var GITHUB_USER = 'iamtonybologna';
 var GITHUB_TOKEN = 'b1d448a48b1e6f52b5303b1f899e2c7f3d179478';
 
 console.log('Welcome to the GitHub Avatar Downloader!');
 
 function getRepoContributors(repoOwner, repoName, cb) {
-  request.get('https://github.com/' + repoOwner + '/' + repoName)
-       .on('error', function (err) {
-         throw err;
-       })
-       .on('response', function (response) {
-         console.log('Response Status Code: ', response.statusCode);
-       })
-       // .pipe(fs.createWriteStream())
-       .on('finish', function() {
-        console.log('Process complete.');
-      });
   var requestURL = 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
-  console.log(requestURL);
-}
 
-getRepoContributors("jquery", "jquery", function(err, result) {
+  var options = {
+    url: requestURL,
+    headers: {
+      'User-Agent': 'GitHub Avatar Downloader - Student Project'
+    }
+  };
+
+  request.get(options, function(error, response, body) {
+    if (!error && response.statusCode === 200) {
+      console.log(JSON.parse(body));
+    };
+  })
+
+};
+
+  getRepoContributors("jquery", "jquery", function(err, result) {
   console.log("Errors:", err);
   console.log("Result:", result);
 });
